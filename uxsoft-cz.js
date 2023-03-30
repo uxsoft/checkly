025daca4fe4e458c3c0ec65267ddc2dfa8d6f5de
Jan Dryk <me@uxsoft.cz>
2023-03-30 09:40:58 +0200

    Wait for logo on uxsoft.cz

--- uxsoft-cz.js
+++ uxsoft-cz.js
@@ -2,6 +2,7 @@ const { expect, test } = require('@playwright/test')
 
 test('visit page and take screenshot', async ({ page }) => {
     const response = await page.goto(process.env.ENVIRONMENT_URL || 'https://uxsoft.cz')
+    await page.waitForSelector("#logo")
 
     expect(response.status()).toBeLessThan(400)
     await page.screenshot({ path: 'screenshot.jpg' })
